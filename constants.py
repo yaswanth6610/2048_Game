6b78544ccae38657e63c5dc8091f561315eef853
kotha-aravind0698 <venkat.aravind0698@outlook.com>
2023-05-31 00:06:45 +0530

    added yaml file and Github actions workflow

--- constants.py
+++ constants.py
@@ -4,10 +4,10 @@ SIZE = 400
 GRID_LEN = 4
 GRID_PADDING = 10
 
-BACKGROUND_COLOR_GAME = "#9AB32D"
-BACKGROUND_COLOR_CELL_EMPTY = "#1CC9BE"
+BACKGROUND_COLOR_GAME = "#92877d"
+BACKGROUND_COLOR_CELL_EMPTY = "#9e948a"
 
-BACKGROUND_COLOR_DICT = {2: "#3c664c", 4: "#ede0c8", 8: "#f2b179",
+BACKGROUND_COLOR_DICT = {2: "#eee4da", 4: "#ede0c8", 8: "#f2b179",
                          16: "#f59563", 32: "#f67c5f", 64: "#f65e3b",
                          128: "#edcf72", 256: "#edcc61", 512: "#edc850",
                          1024: "#edc53f", 2048: "#edc22e",
